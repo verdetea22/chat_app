bf9cc24b9f6f4e20854e492d8e3b36b16bc23d23
mmJDV <mmount@jdvproducts.com>
2024-10-29 11:20:39 -0400

    loading screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/screens/splash.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -31,6 +32,10 @@ class MyApp extends StatelessWidget {
       home: StreamBuilder(
         stream: FirebaseAuth.instance.authStateChanges(), 
         builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting){
+            return const SplashScreen();
+          }
+
           if (snapshot.hasData) {
             return const ChatScreen();
           }
